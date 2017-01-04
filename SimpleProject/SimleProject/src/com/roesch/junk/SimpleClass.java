ba145710e347b958da6a045e25dfb2f75cfa1c7f
Douglas Roesch <Douglas.Roesch@bnymellon.com>
2017-01-04 17:42:43 -0500

    Altered message to label 'other' branch.

--- SimpleProject/SimleProject/src/com/roesch/junk/SimpleClass.java
+++ SimpleProject/SimleProject/src/com/roesch/junk/SimpleClass.java
@@ -3,7 +3,7 @@ package com.roesch.junk;
 public class SimpleClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from SimpleClass, one more time.");
+		System.out.println("Hello from SimpleClass, from the other branch.");
 
 	}
 
